61cf9e5a25c92356a7519575f4a3a997eabcecaf
teamthefour <51492907+teamthefour@users.noreply.github.com>
2019-06-29 07:48:51 +0200

    Add files via upload

---
+++ web-go-ahead/rtl/assets/dist/active-switcher.js
@@ -0,0 +1,20 @@
+
+var colorSheets = [
+    {
+        color: "#506CEA",
+        title: "Switch to Default",
+        href: "./assets/css/color/color-default.css"
+    },
+    {
+        color: "#003262",
+        title: "Switch to Prussian Blue",
+        href: "./assets/css/color/color-prussian-blue.css"
+    },
+    {
+        color: "#B3428F",
+        title: "Switch to Purple",
+        href: "./assets/css/color/color-purple.css"
+    }
+];
+
+ColorSwitcher.init(colorSheets);
